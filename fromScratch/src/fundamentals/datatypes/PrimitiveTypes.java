0b0035b748e177f38254f0f42662681f4e06a665
rins <rinsart127@gmail.com>
2025-09-03 16:08:00 +0300

    fundamental-prog-java

---
+++ fromScratch/src/fundamentals/datatypes/PrimitiveTypes.java
@@ -0,0 +1,9 @@
+package fundamentals.datatypes;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        // 1. Primitive Data Types
+        byte smallNumber = 127; // Range: -128 to 127
+        short mediumNumber = 32000; // // Range: -32,768 to 32,767
+    }
+}
---
+++ fromScratch/src/fundamentals/datatypes/Test.java
@@ -0,0 +1,12 @@
+package fundamentals.datatypes;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 1;
+        byte b = 1;
+        short c = 1;
+        long d = 1;
+
+        System.out.println(c + d);
+    }
+}
